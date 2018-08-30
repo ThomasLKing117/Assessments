7a8647b0f5911f17c2131338192fcd2f32ea640b
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-08-30 13:41:54 -0500

    Need to finish

--- NumberGuessingGame/Source.cpp
+++ NumberGuessingGame/Source.cpp
@@ -1,14 +1,80 @@
 #include <iostream>
 #include <cstdlib> 
-#include <ctime> 
 
 int main()
 {
+	// These are the variables im starting with
 	int guess;
 	char input;
-	std::cout << "Hello user!" << std::endl;
-	std::cout << "Enter your number..." << std::endl;
-	std::cin >> guess;
-	
-	system("pause");
+	// This is asking if you want to play
+	std::cout << "Press (+) to play game!" << std::endl;
+	std::cout << "Press (-) to leave..." << std::endl;
+	std::cin >> input;
+	// This is the game its self
+	while (input == '+')
+	{
+		system("cls");
+		// Here I am telling the user about the game and how it works
+		{
+			std::cout << "Hello user, I want you to think of a number!" << std::endl;
+			std::cout << "The number must be between the numbers 1 - 100..." << std::endl;
+			std::cout << "I will then have the computer guess that number..." << std::endl;
+
+			system("pause");
+			system("cls");
+		}
+		//Here I am telling the computer to generate a random number
+		{
+			guess = rand() % 100 + 1;
+		}
+		//This is where the user tells the computer whether or not its guess was to high, low, or equal to the users guess
+		{
+			std::cout << "Computer guess, " << guess << std::endl;
+			std::cout << "Is the guess eqaul to(=), less than(<), or greater than(>), your number?" << std::endl;
+			std::cin >> input;
+
+			while (input != '=')
+			{
+				if (input == '<')
+				{
+
+				}
+				else if (input == '>')
+				{
+
+				}
+				else // This happens if the respones given by the user is invalid
+				{
+					std::cout << "Invlaid response" << std::endl;
+					system("pause");
+					return 0;
+				}
+			}
+			system("cls");
+		}
+		// This is the end of the game
+		{
+			if (input == '=')
+			{
+				std::cout << "The computer has guessed the number..." << std::endl;
+				std::cout << "The game is now over" << std::endl;
+				system("pause");
+				system("cls");
+			}
+			// This ask the user is they want to paly again or quit
+			std::cout << "Do you want to play again?" << std::endl;
+			std::cout << "I yes press (+) if no press (-)" << std::endl;
+			std::cin >> input;
+
+			system("cls");
+
+			std::cout << "Thanks for playing!!!" << std::endl;
+			system("Pause");
+		}
+	}
+	// This ends the game 
+	while (input == '-')
+	{
+		return 0;
+	}
 }
