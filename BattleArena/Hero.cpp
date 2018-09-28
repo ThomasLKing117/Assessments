1d3bf7777d6d8d50b793a687fb4c1cfeefe9f4fc
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-09-28 11:38:37 -0500

    Added BattleArena

---
+++ BattleArena/Hero.cpp
@@ -0,0 +1,43 @@
+#include "Hero.h"
+
+Hero::Hero()
+{
+	mHealth = 100;
+	mPower = rand() % 100 + 1;
+	mName = new char[255];
+}
+
+void Hero::GetName(const char* name)
+{
+	mName = name;
+}
+
+void Hero::PrintName()
+{
+	std::cout << mName;
+}
+
+void Hero::Fight(Hero* hero)
+{
+	hero->TakeDamage(mPower);
+}
+
+bool Hero::IsAlive()
+{
+	return mHealth > 0;
+}
+
+void Hero::TakeDamage(int amount)
+{
+	mHealth -= amount;
+}
+
+int Hero::GetHealth()
+{
+	return mHealth;
+}
+
+int Hero::GetPower()
+{
+	return mPower;
+}
